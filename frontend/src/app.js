87a0d0a017dd17d587a8fcbe9930932a75d6d1e1
Kathrin Eichinger <kathrin801@gmail.com>
2021-03-21 11:23:24 +0000

    navbar

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import './styles/root.scss'
 
+import MenuBar from './components/Menu'
 import Home from './components/Home'
 import ColourIndex from './components/ColourIndex'
 
@@ -12,6 +13,7 @@ const App = () => (
 
   <BrowserRouter>
     <CssBaseline />
+    <MenuBar />
     <Switch>
       <Route exact path='/' component={Home} />
       <Route exact path='/colours' component={ColourIndex} />
---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import ArrowBackIosOutlinedIcon from '@material-ui/icons/ArrowBackIosOutlined'
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined'
+import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined'
+import ListAltOutlinedIcon from '@material-ui/icons/ListAltOutlined'
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined'
+import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined'
+import { Link, withRouter } from 'react-router-dom'
+
+import '../styles/components/Menu.scss'
+
+import Auth from '../lib/Auth'
+
+function MenuBar(props) {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  function handleLogout() {
+    Auth.logout()
+    props.history.push('/')
+    setAnchorEl(null)
+  }
+
+  return (
+    <div id="menu">
+      <IconButton id="menu-button" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        <ArrowBackIosOutlinedIcon className="icon" />
+      </IconButton>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>
+          <Link to="/"><HomeOutlinedIcon /></Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link to="/colours"><ListAltOutlinedIcon /></Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link to="/colours/new"><AddCircleOutlineOutlinedIcon /></Link>
+        </MenuItem>
+        {!Auth.isAuthorized() && <MenuItem onClick={handleClose}>
+          <Link to="/account"><AccountCircleOutlinedIcon /></Link>
+        </MenuItem>}
+        {Auth.isAuthorized() && <div className="navbar-item">
+          <a onClick={() => handleLogout()}><ExitToAppOutlinedIcon /></a>
+        </div>}
+      </Menu>
+    </div>
+  )
+}
+
+export default withRouter(MenuBar)
